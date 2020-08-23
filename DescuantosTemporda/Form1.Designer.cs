6b4945e24afa25e707cabc1bd96b0ac83a72c912
Ishwar Lopez Rua <ilopezr101@gmail.com>
2020-08-23 17:27:23 -0500

    Programa con el cual se utiliza para las ventas de temporada para una tienda segun (temporada y % de descueto aplicado.

---
+++ DescuantosTemporda/Form1.Designer.cs
@@ -0,0 +1,246 @@
+﻿namespace DescuantosTemporda
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtValor = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rbDiciembre = new System.Windows.Forms.RadioButton();
+            this.rbHalloween = new System.Windows.Forms.RadioButton();
+            this.rbEscolar = new System.Windows.Forms.RadioButton();
+            this.btnIngresar = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.btnRestaurarCampos = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(36, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(428, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Descuentos por tempodara";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold);
+            this.label2.Location = new System.Drawing.Point(117, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(220, 24);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Seleccione temporada";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(3, 278);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(137, 24);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Ingrese valor:";
+            // 
+            // txtValor
+            // 
+            this.txtValor.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtValor.Location = new System.Drawing.Point(146, 274);
+            this.txtValor.Name = "txtValor";
+            this.txtValor.Size = new System.Drawing.Size(158, 31);
+            this.txtValor.TabIndex = 5;
+            this.txtValor.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.rbDiciembre);
+            this.groupBox1.Controls.Add(this.rbHalloween);
+            this.groupBox1.Controls.Add(this.rbEscolar);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(149, 126);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(155, 127);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Temporda";
+            // 
+            // rbDiciembre
+            // 
+            this.rbDiciembre.AutoSize = true;
+            this.rbDiciembre.Location = new System.Drawing.Point(6, 82);
+            this.rbDiciembre.Name = "rbDiciembre";
+            this.rbDiciembre.Size = new System.Drawing.Size(99, 20);
+            this.rbDiciembre.TabIndex = 2;
+            this.rbDiciembre.Text = "DICIEMBRE";
+            this.rbDiciembre.UseVisualStyleBackColor = true;
+            // 
+            // rbHalloween
+            // 
+            this.rbHalloween.AutoSize = true;
+            this.rbHalloween.Location = new System.Drawing.Point(7, 56);
+            this.rbHalloween.Name = "rbHalloween";
+            this.rbHalloween.Size = new System.Drawing.Size(110, 20);
+            this.rbHalloween.TabIndex = 1;
+            this.rbHalloween.Text = "HALLOWEEN";
+            this.rbHalloween.UseVisualStyleBackColor = true;
+            // 
+            // rbEscolar
+            // 
+            this.rbEscolar.AutoSize = true;
+            this.rbEscolar.Checked = true;
+            this.rbEscolar.Location = new System.Drawing.Point(7, 30);
+            this.rbEscolar.Name = "rbEscolar";
+            this.rbEscolar.Size = new System.Drawing.Size(89, 20);
+            this.rbEscolar.TabIndex = 0;
+            this.rbEscolar.TabStop = true;
+            this.rbEscolar.Text = "ESCOLAR";
+            this.rbEscolar.UseVisualStyleBackColor = true;
+            // 
+            // btnIngresar
+            // 
+            this.btnIngresar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnIngresar.Location = new System.Drawing.Point(342, 255);
+            this.btnIngresar.Name = "btnIngresar";
+            this.btnIngresar.Size = new System.Drawing.Size(105, 64);
+            this.btnIngresar.TabIndex = 7;
+            this.btnIngresar.Text = "Ingresar";
+            this.btnIngresar.UseVisualStyleBackColor = true;
+            this.btnIngresar.Click += new System.EventHandler(this.btnIngresar_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(152, 394);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(66, 24);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "label4";
+            this.label4.Visible = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(151, 361);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(66, 24);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "label5";
+            this.label5.Visible = false;
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(77, 394);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(68, 24);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Total :";
+            this.label6.Visible = false;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(29, 361);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(116, 24);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Descuento:";
+            this.label7.Visible = false;
+            // 
+            // btnRestaurarCampos
+            // 
+            this.btnRestaurarCampos.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRestaurarCampos.Location = new System.Drawing.Point(342, 354);
+            this.btnRestaurarCampos.Name = "btnRestaurarCampos";
+            this.btnRestaurarCampos.Size = new System.Drawing.Size(105, 64);
+            this.btnRestaurarCampos.TabIndex = 12;
+            this.btnRestaurarCampos.Text = "Restaurar Campos";
+            this.btnRestaurarCampos.UseVisualStyleBackColor = true;
+            this.btnRestaurarCampos.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.CadetBlue;
+            this.ClientSize = new System.Drawing.Size(490, 450);
+            this.Controls.Add(this.btnRestaurarCampos);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnIngresar);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.txtValor);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Descuentos ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtValor;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rbDiciembre;
+        private System.Windows.Forms.RadioButton rbHalloween;
+        private System.Windows.Forms.RadioButton rbEscolar;
+        private System.Windows.Forms.Button btnIngresar;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button btnRestaurarCampos;
+    }
+}
+
---
+++ DescuantosTemporda/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DescuantosTemporda
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnIngresar_Click(object sender, EventArgs e)
+        {
+            double valor = 0.0;
+
+            double descuento = 0.0;
+
+
+            valor = Convert.ToDouble(txtValor.Text);
+
+            if ((rbEscolar.Checked == true) && (valor <= 100000))
+            {
+                descuento = 0.0;
+                valor = valor + descuento;
+
+            }
+
+            else if ((rbEscolar.Checked == true) && (valor >= 100001 && valor <= 300000))
+            {
+                descuento = valor * 0.10;
+                valor = valor - descuento;
+
+            }
+
+            else if ((rbEscolar.Checked == true) && (valor >300000))
+            {
+                descuento = valor * 0.15;
+                valor = valor - descuento;
+
+            }
+
+
+ 
+
+            
+            if ((rbHalloween.Checked == true) && (valor >= 50 && valor <= 30000))
+            {
+                descuento = 0.0;
+                valor = valor + descuento;
+            }
+
+            else if ((rbHalloween.Checked == true) && (valor >= 30001 && valor <= 200000))
+            {
+                descuento = valor * 0.02;
+                valor = valor - descuento;
+            }
+
+            else if ((rbHalloween.Checked == true) && (valor > 200000))
+            {
+                descuento = valor * 0.05;
+                valor = valor - descuento;
+            }
+
+
+            
+
+            if ((rbDiciembre.Checked == true) && (valor >= 50 && valor <= 300000))
+            {
+                descuento = 0.0;
+                valor = valor + descuento;
+
+            }
+
+            else if ((rbDiciembre.Checked == true) && (valor >= 300001 && valor <= 500000))
+            {
+                descuento = valor * 0.05;
+                valor = valor - descuento;
+
+            }
+
+            else if ((rbDiciembre.Checked == true) && (valor > 500000))
+            {
+                descuento = valor * 0.10;
+                valor = valor - descuento;
+
+
+            }
+            label4.Visible = true;
+            label4.Text = Convert.ToString(String.Format("{0:n0}", valor));
+            label5.Visible = true;
+            label5.Text = Convert.ToString(String.Format("{0:n0}", descuento));
+            label6.Visible = true;
+            label7.Visible = true;
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txtValor.Text = String.Empty;
+            label4.Visible = false;
+            label5.Visible = false;
+            label6.Visible = false;
+            label7.Visible = false;
+            txtValor.Focus();
+            rbEscolar.Checked = true;
+        }
+    }
+}
